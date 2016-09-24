b8b7bf80f57b25bd8324872a205c9fa44a7eb5cf
Namdak Tonpa <maxim@synrc.com>
2016-09-24 21:35:20 +0300

    cleanup

--- src/bcd.erl
+++
@@ -1,7 +0,0 @@
--module(bcd).
--compile(export_all).
-
-decode(Bits) -> list_to_integer([X+$0 || <<X:4>> <= Bits]).
-encode(N, S) when N >= 0, S > 0 -> << <<(X-$0):4>>
-               || X <- tl(integer_to_list(trunc(math:pow(10,S*2)) + (N rem trunc(math:pow(10,S*2))))) >>.
-
